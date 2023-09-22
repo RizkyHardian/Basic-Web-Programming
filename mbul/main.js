e337aa9b938e3bfb072bd4be5daee6133cb681f7
Rizky Hardian <92320728+RizkyHardian@users.noreply.github.com>
2023-09-23 03:39:33 +0800

    Add Color Pallete, Responsive Layout, Flexbox

---
+++ mbul/main.js
@@ -0,0 +1,16 @@
+var tombol = document.getElementById("tombol");
+var main = document.querySelector("main");
+tombol.addEventListener('click',function(){
+    main.classList.toggle("dark")
+    alert("Ganti tema cuyyyy!!!");
+})
+
+const lnlist = document.getElementById("lnlist");
+lnlist.addEventListener('click',function(){
+    alert("Belum ada list light novelnya cuyy");
+})
+
+const wnlist = document.getElementById("wnlist");
+wnlist.addEventListener('click',function(){
+    alert("Belum ada list web novelnya cuyy");
+})
